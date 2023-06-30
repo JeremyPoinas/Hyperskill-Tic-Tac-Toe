e5cde6608f50ab2f152e3a6f3a95d16b5a510187
Jeremy Poinas <poinas.jeremy@gmail.com>
2023-06-30 11:42:33 +0200

    Started project

---
+++ Simple Tic-Tac-Toe (Java)/task/src/tictactoe/Main.java
@@ -0,0 +1,12 @@
+package tictactoe;
+
+public class Main {
+    public static void main(String[] args) {
+        String grid = """
+                X O X
+                O X O
+                X X O
+                """;
+        System.out.println(grid);
+    }
+}
