49cf7c9de5351f67254786a2ce27421d08579a0b
Jeremy Poinas <poinas.jeremy@gmail.com>
2023-07-03 14:18:00 +0200

    Print tic-tac-toe entry

--- Simple Tic-Tac-Toe (Java)/task/src/tictactoe/Main.java
+++ Simple Tic-Tac-Toe (Java)/task/src/tictactoe/Main.java
@@ -1,12 +1,29 @@
 package tictactoe;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        char firstSymbol = input.charAt(0);
+        char secondSymbol = input.charAt(1);
+        char thirdSymbol = input.charAt(2);
+        char fourthSymbol = input.charAt(3);
+        char fifthSymbol = input.charAt(4);
+        char sixthSymbol = input.charAt(5);
+        char seventhSymbol = input.charAt(6);
+        char eighthSymbol = input.charAt(7);
+        char ninthSymbol = input.charAt(8);
+
+
         String grid = """
-                X O X
-                O X O
-                X X O
+                ---------
+                | %s %s %s |
+                | %s %s %s |
+                | %s %s %s |
+                ---------
                 """;
-        System.out.println(grid);
+        System.out.printf(grid, firstSymbol, secondSymbol, thirdSymbol, fourthSymbol, fifthSymbol, sixthSymbol, seventhSymbol, eighthSymbol, ninthSymbol);
     }
 }
