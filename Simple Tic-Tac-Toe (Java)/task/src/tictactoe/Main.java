896b733ed46736bad16cdec8204cb39f63bdea56
Jeremy Poinas <poinas.jeremy@gmail.com>
2023-07-04 16:12:57 +0200

    Check game status

--- Simple Tic-Tac-Toe (Java)/task/src/tictactoe/Main.java
+++ Simple Tic-Tac-Toe (Java)/task/src/tictactoe/Main.java
@@ -4,26 +4,63 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        char[] input = getInput();
+        printBoard(input);
+        System.out.println(getState(input));
+    }
+
+    private static char[] getInput() {
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.next();
-        char firstSymbol = input.charAt(0);
-        char secondSymbol = input.charAt(1);
-        char thirdSymbol = input.charAt(2);
-        char fourthSymbol = input.charAt(3);
-        char fifthSymbol = input.charAt(4);
-        char sixthSymbol = input.charAt(5);
-        char seventhSymbol = input.charAt(6);
-        char eighthSymbol = input.charAt(7);
-        char ninthSymbol = input.charAt(8);
+        System.out.print("Enter cells: ");
+        return scanner.nextLine().toCharArray();
+    }
 
+    private static void printBoard(char[] input) {
+        System.out.println("---------");
+        System.out.printf("| %c %c %c |%n", input[0], input[1], input[2]);
+        System.out.printf("| %c %c %c |%n", input[3], input[4], input[5]);
+        System.out.printf("| %c %c %c |%n", input[6], input[7], input[8]);
+        System.out.println("---------");
+    }
 
-        String grid = """
-                ---------
-                | %s %s %s |
-                | %s %s %s |
-                | %s %s %s |
-                ---------
-                """;
-        System.out.printf(grid, firstSymbol, secondSymbol, thirdSymbol, fourthSymbol, fifthSymbol, sixthSymbol, seventhSymbol, eighthSymbol, ninthSymbol);
+    private static String getState(char[] input) {
+        int totalX = 0;
+        int totalO = 0;
+        for (char symbol : input) {
+            if (symbol == 'X') {
+                totalX++;
+            } else if (symbol == 'O') {
+                totalO++;
+            }
+        }
+        int difference = totalO - totalX;
+        if (difference < -1 || difference > 1) {
+            return "Impossible";
+        }
+        boolean xWon = isSymbolWon(input, 'X');
+        boolean oWon = isSymbolWon(input, 'O');
+        if (xWon && oWon) {
+            return "Impossible";
+        }
+        if (xWon) {
+            return "X wins";
+        }
+        if (oWon) {
+            return "O wins";
+        }
+        if (totalO + totalX == 9) {
+            return "Draw";
+        }
+        return "Game not finished";
+    }
+    private static boolean isSymbolWon(char[] input, char symbol) {
+        return input[0] == symbol && input[1] == symbol && input[2] == symbol
+                || input[3] == symbol && input[4] == symbol && input[5] == symbol
+                || input[6] == symbol && input[7] == symbol && input[8] == symbol
+                || input[0] == symbol && input[3] == symbol && input[6] == symbol
+                || input[1] == symbol && input[4] == symbol && input[7] == symbol
+                || input[2] == symbol && input[5] == symbol && input[8] == symbol
+                || input[0] == symbol && input[4] == symbol && input[8] == symbol
+                || input[2] == symbol && input[4] == symbol && input[6] == symbol;
     }
 }
