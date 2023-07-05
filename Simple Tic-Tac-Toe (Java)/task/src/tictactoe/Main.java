9de32ae3fab7b0a71eb7f6723f342c4b24b9646d
Jeremy Poinas <poinas.jeremy@gmail.com>
2023-07-05 11:28:08 +0200

    Check game status

--- Simple Tic-Tac-Toe (Java)/task/src/tictactoe/Main.java
+++ Simple Tic-Tac-Toe (Java)/task/src/tictactoe/Main.java
@@ -1,44 +1,95 @@
 package tictactoe;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        char[] input = getInput();
-        printBoard(input);
-        System.out.println(getState(input));
+        char[][] grid = getGrid();
+        printBoard(grid);
+        String status = getState(grid);
+        boolean xSign = true;
+
+        while (status.equals("Game not finished")) {
+            int[] move =  getInput();
+            while (!checkInput(move, grid)) {
+                move = getInput();
+            }
+            if (move != null) {
+                int xCoord = move[0] - 1;
+                int yCoord = move[1] - 1;
+                grid[xCoord][yCoord] = xSign ? 'X' : 'O';
+                xSign = !xSign;
+            }
+            printBoard(grid);
+            status = getState(grid);
+        }
+        System.out.println(status);
     }
 
-    private static char[] getInput() {
+    public static  boolean checkInput(int[] move, char[][] grid) {
+        if (move == null) {
+            System.out.println("You should enter numbers!");
+            return false;
+        }
+        if (move[0] < 1 || move[0] > 3 || move[1] < 1 || move[1] > 3) {
+            System.out.println("Coordinates should be from 1 to 3!");
+            return false;
+        }
+        if (grid[move[0] - 1][move[1] - 1] != ' ') {
+            System.out.println("This cell is occupied! Choose another one!");
+            return false;
+        }
+        return true;
+    }
+
+    public static  int[] getInput() {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter cells: ");
-        return scanner.nextLine().toCharArray();
+        System.out.print("Enter the coordinates you want to play: ");
+        if (scanner.hasNextInt()) {
+            int xCoord = scanner.nextInt();
+            if (scanner.hasNextInt()) {
+                int yCoord = scanner.nextInt();
+                return new int[] {xCoord, yCoord};
+            }
+        }
+        return null;
     }
 
-    private static void printBoard(char[] input) {
+    private static char[][] getGrid() {
+        char[][] grid = new char[3][3];
+        for (char[] line: grid) {
+            Arrays.fill(line, ' ');
+        }
+        return grid;
+    }
+
+    private static void printBoard(char[][] grid) {
         System.out.println("---------");
-        System.out.printf("| %c %c %c |%n", input[0], input[1], input[2]);
-        System.out.printf("| %c %c %c |%n", input[3], input[4], input[5]);
-        System.out.printf("| %c %c %c |%n", input[6], input[7], input[8]);
+        System.out.printf("| %c %c %c |%n", grid[0][0], grid[0][1], grid[0][2]);
+        System.out.printf("| %c %c %c |%n", grid[1][0], grid[1][1], grid[1][2]);
+        System.out.printf("| %c %c %c |%n", grid[2][0], grid[2][1], grid[2][2]);
         System.out.println("---------");
     }
 
-    private static String getState(char[] input) {
+    private static String getState(char[][] grid) {
         int totalX = 0;
         int totalO = 0;
-        for (char symbol : input) {
-            if (symbol == 'X') {
-                totalX++;
-            } else if (symbol == 'O') {
-                totalO++;
+        for (char[] line : grid) {
+            for (char symbol : line) {
+                if (symbol == 'X') {
+                    totalX++;
+                } else if (symbol == 'O') {
+                    totalO++;
+                }
             }
         }
         int difference = totalO - totalX;
         if (difference < -1 || difference > 1) {
             return "Impossible";
         }
-        boolean xWon = isSymbolWon(input, 'X');
-        boolean oWon = isSymbolWon(input, 'O');
+        boolean xWon = isSymbolWon(grid, 'X');
+        boolean oWon = isSymbolWon(grid, 'O');
         if (xWon && oWon) {
             return "Impossible";
         }
@@ -53,14 +104,14 @@ public class Main {
         }
         return "Game not finished";
     }
-    private static boolean isSymbolWon(char[] input, char symbol) {
-        return input[0] == symbol && input[1] == symbol && input[2] == symbol
-                || input[3] == symbol && input[4] == symbol && input[5] == symbol
-                || input[6] == symbol && input[7] == symbol && input[8] == symbol
-                || input[0] == symbol && input[3] == symbol && input[6] == symbol
-                || input[1] == symbol && input[4] == symbol && input[7] == symbol
-                || input[2] == symbol && input[5] == symbol && input[8] == symbol
-                || input[0] == symbol && input[4] == symbol && input[8] == symbol
-                || input[2] == symbol && input[4] == symbol && input[6] == symbol;
+    private static boolean isSymbolWon(char[][] grid, char symbol) {
+        return grid[0][0] == symbol && grid[0][1] == symbol && grid[0][2] == symbol
+                || grid[1][0] == symbol && grid[1][1] == symbol && grid[1][2] == symbol
+                || grid[2][0] == symbol && grid[2][1] == symbol && grid[2][2] == symbol
+                || grid[0][0] == symbol && grid[1][0] == symbol && grid[2][0] == symbol
+                || grid[0][1] == symbol && grid[1][1] == symbol && grid[2][1] == symbol
+                || grid[0][2] == symbol && grid[1][2] == symbol && grid[2][2] == symbol
+                || grid[0][0] == symbol && grid[1][1] == symbol && grid[2][2] == symbol
+                || grid[0][2] == symbol && grid[1][1] == symbol && grid[2][0] == symbol;
     }
 }
